d228b1dcaa4491cfd8e6b9607531f1ad7fe559e0
hahiep205 <145806640+hahiep205@users.noreply.github.com>
2024-10-12 22:15:28 +0700

    Create QuanLyDichVu.java
    class quản lý dịch vụ

---
+++ service/QuanLyDichVu.java
@@ -0,0 +1,35 @@
+package service;
+
+import model.DichVu;
+import java.util.ArrayList;
+
+public class QuanLyDichVu {
+    private ArrayList<DichVu> DanhSachDichVu;
+
+    public QuanLyDichVu() {
+        DanhSachDichVu = new ArrayList<>();
+        DanhSachDichVu.add(new DichVu("Ăn sáng", 100000));
+        DanhSachDichVu.add(new DichVu("Đưa đón", 200000));
+        DanhSachDichVu.add(new DichVu("Giặt ủi", 50000));
+        DanhSachDichVu.add(new DichVu("Spa", 300000));
+    }
+
+    public void HienThiDichVu() {
+        for (int i = 0; i < DanhSachDichVu.size(); i++) {
+            DichVu dv = DanhSachDichVu.get(i);
+            System.out.println((i + 1) + ": " + dv.getTenDichVu() + " - Giá tiền: " + dv.getGia());
+        }
+    }
+
+    public DichVu TimKiemDichVu(String TenDichVu) {
+        for (int i = 0; i < DanhSachDichVu.size(); i++) {
+            DichVu dv = DanhSachDichVu.get(i);
+            if (dv.getTenDichVu().equalsIgnoreCase(TenDichVu)) {
+                return dv;
+            }
+        }
+        return null;
+    }
+
+
+}
