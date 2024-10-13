c62cd13bf6d5c110892a7977ac9af56ad07d36e3
hahiep205 <145806640+hahiep205@users.noreply.github.com>
2024-10-13 23:21:04 +0700

    Update PhongDon.java

--- model/PhongDon.java
+++ model/PhongDon.java
@@ -1,12 +1,16 @@
 package model;
 
 public class PhongDon extends Phong {
+    // Giá cố định cho phòng đơn
     private static final double GIA = 300000;
 
+    // Constructor gọi constructor của lớp cha Phong
     public PhongDon(String SoPhong, String TrangThai) {
-        super(SoPhong, "Phòng đơn", TrangThai);
+        super(SoPhong, "Simple Room", TrangThai, GIA);
     }
 
+    // Ghi đè phương thức getGia từ lớp cha để trả về giá của phòng đơn
+    @Override
     public double getGia() {
         return GIA;
     }
