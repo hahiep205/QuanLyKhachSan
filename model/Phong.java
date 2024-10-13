3325b190ce266f1d393c28a93248a5cadcc954f4
hahiep205 <145806640+hahiep205@users.noreply.github.com>
2024-10-13 23:19:38 +0700

    Update Phong.java

--- model/Phong.java
+++ model/Phong.java
@@ -6,12 +6,20 @@ public class Phong {
     private String TrangThai;
     private double gia;
 
-    public Phong(String SoPhong, String LoaiPhong, String TrangThai) {
+    public Phong(String SoPhong, String LoaiPhong, String TrangThai, double gia) {
         this.SoPhong = SoPhong;
         this.LoaiPhong = LoaiPhong;
         this.TrangThai = TrangThai;
+        this.gia = gia;
     }
 
+    public double getGia() {
+        return gia;
+    }
+
+    public void setGia(double gia) {
+        this.gia = gia;
+    }
 
     public String getSoPhong() {
         return SoPhong;
@@ -36,7 +44,8 @@ public class Phong {
     public void setTrangThai(String TrangThai) {
         this.TrangThai = TrangThai;
     }
-    
+
+    // Phương thức tính tiền dựa trên số ngày
     public double tinhTien(int soNgay) {
         return gia * soNgay;
     }
