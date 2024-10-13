bf283105b58df10d6a6672e882fac2c29e148c05
hahiep205 <145806640+hahiep205@users.noreply.github.com>
2024-10-13 23:21:22 +0700

    Update PhongVIP.java

--- model/PhongVIP.java
+++ model/PhongVIP.java
@@ -1,12 +1,16 @@
 package model;
 
 public class PhongVIP extends Phong {
+    // Giá cố định cho phòng VIP
     private static final double GIA = 1000000;
 
+    // Constructor gọi constructor của lớp cha Phong
     public PhongVIP(String SoPhong, String TrangThai) {
-        super(SoPhong, "Phòng VIP", TrangThai);
+        super(SoPhong, "VIP Room", TrangThai, GIA);
     }
 
+    // Ghi đè phương thức getGia từ lớp cha để trả về giá của phòng VIP
+    @Override
     public double getGia() {
         return GIA;
     }
