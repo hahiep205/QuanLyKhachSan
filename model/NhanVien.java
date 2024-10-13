935cc3b4244ec6cfe522ebf9cfa981a04490d557
hahiep205 <145806640+hahiep205@users.noreply.github.com>
2024-10-13 23:19:14 +0700

    Update NhanVien.java

--- model/NhanVien.java
+++ model/NhanVien.java
@@ -1,52 +1,72 @@
 package model;
 
-public class Phong {
-    private String SoPhong;
-    private String LoaiPhong;
-    private String TrangThai;
-    private double gia;
+public class NhanVien {
+    private String ten;
+    private String queQuan;
+    private String sdt;
+    private String cccd;
+    private String soTaiKhoan;
 
-    public Phong(String SoPhong, String LoaiPhong, String TrangThai, double gia) {
-        this.SoPhong = SoPhong;
-        this.LoaiPhong = LoaiPhong;
-        this.TrangThai = TrangThai;
-        this.gia = gia;
+    public NhanVien(String ten, String queQuan, String sdt, String cccd, String soTaiKhoan) {
+        this.ten = ten;
+        this.queQuan = queQuan;
+        this.sdt = sdt;
+        this.cccd = cccd;
+        this.soTaiKhoan = soTaiKhoan;
     }
 
-    public double getGia() {
-        return gia;
+    public String getTen() {
+        return ten;
     }
 
-    public void setGia(double gia) {
-        this.gia = gia;
+    public void setTen(String ten) {
+        this.ten = ten;
     }
 
-    public String getSoPhong() {
-        return SoPhong;
+    public String getQueQuan() {
+        return queQuan;
     }
 
-    public void setSoPhong(String SoPhong) {
-        this.SoPhong = SoPhong;
+    public void setQueQuan(String queQuan) {
+        this.queQuan = queQuan;
     }
 
-    public String getLoaiPhong() {
-        return LoaiPhong;
+    public String getSdt() {
+        return sdt;
     }
 
-    public void setLoaiPhong(String LoaiPhong) {
-        this.LoaiPhong = LoaiPhong;
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
     }
 
-    public String getTrangThai() {
-        return TrangThai;
+    public String getCccd() {
+        return cccd;
     }
 
-    public void setTrangThai(String TrangThai) {
-        this.TrangThai = TrangThai;
+    public void setCccd(String cccd) {
+        this.cccd = cccd;
     }
 
-    // Phương thức tính tiền dựa trên số ngày
-    public double tinhTien(int soNgay) {
-        return gia * soNgay;
+    public String getSoTaiKhoan() {
+        return soTaiKhoan;
+    }
+
+    public void setSoTaiKhoan(String soTaiKhoan) {
+        this.soTaiKhoan = soTaiKhoan;
+    }
+
+    // Phương thức kiểm tra tính hợp lệ của thông tin tài khoản (so sánh tên và số tài khoản)
+    public boolean kiemTraTaiKhoan(String ten, String soTaiKhoan) {
+        return this.ten.equalsIgnoreCase(ten) && this.soTaiKhoan.equals(soTaiKhoan);
+    }
+
+    // Phương thức hiển thị thông tin nhân viên
+    @Override
+    public String toString() {
+        return "Tên: " + ten +
+               " - Quê quán: " + queQuan +
+               " - Số điện thoại: " + sdt +
+               " - CCCD: " + cccd +
+               " - Số tài khoản: " + soTaiKhoan;
     }
 }
